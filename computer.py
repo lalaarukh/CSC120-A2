acc004034f36cdc90faf749f32f81e157521fe6a
lalaarukh <lrukh@smith.edu>
2023-02-07 21:09:17 -0500

    assignment upload

--- computer.py
+++ computer.py
@@ -1,10 +1,25 @@
 class Computer:
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
 
-    # What attributes will it need?
+    def __init__(self, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
+        self.description = description
+        self.processor_type - processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made =year_made
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def update_price(self, new_price):
+        self.price = new_price
 
-    # What methods will you need?
+    def update_operating_system(self, operating_system):
+        self.operating_system = operating_system
+
+
+   
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,45 @@
 class ResaleShop:
-
+    inventory = []
+    name = ""
     # What attributes will it need?
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, inventory, name):
+        self.inventory = inventory
+        self.name = name
+        
+    def buy(self, computer):
+        self.inventory.append(computer)
+        
+    def update_price(self, computer, new_price):
+        if computer in self.inventory:
+            self.new_price = new_price
+        else:
+            print(computer, "not found in inventory")
+
+    def sell(self, sell):
+       # self.sell = sell
+       if sell in self.inventory:
+        del self.inventory
+       else:
+        print(sell, "not in inventory")
+
+    def print_inventory(self):
+        if len(self.inventory) != 0:
+            print(self.inventory)
+        else:
+            print("No inventory to display")
+        
 
     # What methods will you need?
+
+comp1 = ResaleShop([], "Lala")
+
+comp1.buy("Macbok Air 2013")
+
+comp1.buy("Macbook Air 2019")
+
+comp1.sell("abc")
+
+comp1.print_inventory()
