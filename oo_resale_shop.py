1a5ad232677d8fee41370a7378d4eb390baf08fd
lalaarukh <lrukh@smith.edu>
2023-03-01 15:30:12 -0500

    updated reflection and rubric

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,23 +1,26 @@
+# set up for resaleshop
 class ResaleShop:
     inventory = []
     name = ""
-    # What attributes will it need?
+   
+   #setting up attributes 
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
     def __init__(self, inventory, name):
         self.inventory = inventory
         self.name = name
-        
+
+    #set up the buy function: add it to computer
     def buy(self, computer):
         self.inventory.append(computer)
-        
+
+    #set up function to update price if in inventory and print warning if not
     def update_price(self, computer, new_price):
         if computer in self.inventory:
             self.new_price = new_price
         else:
             print(computer, "not found in inventory")
 
+    #delete from inventory if selling a computer
     def sell(self, sell):
        # self.sell = sell
        if sell in self.inventory:
@@ -25,6 +28,7 @@ class ResaleShop:
        else:
         print(sell, "not in inventory")
 
+    #print invnetory if inventory is not 0
     def print_inventory(self):
         if len(self.inventory) != 0:
             print(self.inventory)
@@ -32,7 +36,7 @@ class ResaleShop:
             print("No inventory to display")
         
 
-    # What methods will you need?
+    # example run
 
 comp1 = ResaleShop([], "Lala")
 
--- reflection.md
+++ reflection.md
@@ -1,3 +1,4 @@
 Use this file to record your reflection on this assignment. 
 
 What worked, what didn't, what advice would you give someone taking this course in the future?
+It was difficult to get started because my understanding of the attributes and how they work was still very wonky. More guidance as to how to set up the shop would have been helpful but all in all not the worst assignment. Advice would be to get started early!
